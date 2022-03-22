a49decf114d7dd2cb1f5b2bf79a51d7c68164f38
Juv <dev.juv@gmail.com>
2022-03-23 03:40:50 +0900

    chapter02 - val var

---
+++ src/main/kotlin/chapter02/TypeIntro.kt
@@ -0,0 +1,11 @@
+package chapter02
+
+const val MAX_EXPERIENCE = 5000
+
+fun main(vararg args: String) {
+    val playerName: String = "에스트라곤"
+    var experiencePoints: Int = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+}
